94745792972b3f7ec713bc07384e657a269f6f4b
av80r <carl.beek@gmail.com>
2017-07-20 18:06:16 +0200

    Deleted functionality added. Solidity code reflects new deletion fucntionality.

--- SignoDapp/client/main.js
+++ SignoDapp/client/main.js
@@ -2,60 +2,10 @@ import { Template } from "meteor/templating";
 import { Session } from "meteor/session";
 import "./main.html";
 
-ABI_ARRAY = [
-  {
-    constant: true,
-    inputs: [],
-    name: "sundryData",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "certificateIssuer",
-    outputs: [{ name: "", type: "address" }],
-    payable: false,
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "idHash",
-    outputs: [{ name: "", type: "bytes32" }],
-    payable: false,
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: "removeCertificate",
-    outputs: [],
-    payable: false,
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "isDeleted",
-    outputs: [{ name: "", type: "bool" }],
-    payable: false,
-    type: "function"
-  },
-  {
-    inputs: [
-      { name: "hash", type: "bytes32" },
-      { name: "sundry", type: "string" }
-    ],
-    payable: false,
-    type: "constructor"
-  }
-];
-BYTE_CODE =
-  "0x6060604052341561000f57600080fd5b6040516104ae3803806104ae833981016040528080519060200190919080518201919050505b336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550816001816000191690555080600290805190602001906100969291906100ba565b506000600360006101000a81548160ff0219169083151502179055505b505061015f565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f106100fb57805160ff1916838001178555610129565b82800160010185558215610129579182015b8281111561012857825182559160200191906001019061010d565b5b509050610136919061013a565b5090565b61015c91905b80821115610158576000816000905550600101610140565b5090565b90565b6103408061016e6000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063083dfe681461006a5780635455e36b146100f9578063aa4107321461014e578063ba4999e41461017f578063d7efb6b714610194575b600080fd5b341561007557600080fd5b61007d6101c1565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100be5780820151818401525b6020810190506100a2565b50505050905090810190601f1680156100eb5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b341561010457600080fd5b61010c61025f565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b341561015957600080fd5b610161610284565b60405180826000191660001916815260200191505060405180910390f35b341561018a57600080fd5b61019261028a565b005b341561019f57600080fd5b6101a7610301565b604051808215151515815260200191505060405180910390f35b60028054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156102575780601f1061022c57610100808354040283529160200191610257565b820191906000526020600020905b81548152906001019060200180831161023a57829003601f168201915b505050505081565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b60015481565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161415156102e557600080fd5b3373ffffffffffffffffffffffffffffffffffffffff16ff5b5b565b600360009054906101000a900460ff16815600a165627a7a72305820026f73a46b19941134f111c21697a3037468db3499583e2c8f553e7c32b506dd0029";
+ABI_ARRAY = [{"constant":true,"inputs":[],"name":"sundryData","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"certificateIssuer","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"idHash","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"deleteCertificate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"isDeleted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"inputs":[{"name":"hash","type":"bytes32"},{"name":"sundry","type":"string"}],"payable":false,"type":"constructor"}];
+BYTE_CODE = "6060604052341561000f57600080fd5b6040516104b13803806104b1833981016040528080519060200190919080518201919050505b336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550816001816000191690555080600290805190602001906100969291906100ba565b506000600360006101000a81548160ff0219169083151502179055505b505061015f565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f106100fb57805160ff1916838001178555610129565b82800160010185558215610129579182015b8281111561012857825182559160200191906001019061010d565b5b509050610136919061013a565b5090565b61015c91905b80821115610158576000816000905550600101610140565b5090565b90565b6103438061016e6000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063083dfe681461006a5780635455e36b146100f9578063aa4107321461014e578063afa936b81461017f578063d7efb6b714610194575b600080fd5b341561007557600080fd5b61007d6101c1565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100be5780820151818401525b6020810190506100a2565b50505050905090810190601f1680156100eb5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b341561010457600080fd5b61010c61025f565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b341561015957600080fd5b610161610284565b60405180826000191660001916815260200191505060405180910390f35b341561018a57600080fd5b61019261028a565b005b341561019f57600080fd5b6101a7610304565b604051808215151515815260200191505060405180910390f35b60028054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156102575780601f1061022c57610100808354040283529160200191610257565b820191906000526020600020905b81548152906001019060200180831161023a57829003601f168201915b505050505081565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b60015481565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161415156102e557600080fd5b6001600360006101000a81548160ff0219169083151502179055505b5b565b600360009054906101000a900460ff16815600a165627a7a7230582009e651caf5d336c4233a93f7dbf345b82f62d77489c9a9cf4183f789750aacad0029";
 
-Address = web3.eth.accounts[0];
+var Address;
 
 Certificates = new Mongo.Collection("certificates");
 
@@ -103,7 +53,9 @@ function createContract(candidateName, candidateDOB, sundryData) {
 }
 
 function getCertificateFromBlockchain(certificateAddress, template) {
-  myContract = getContract(certificateAddress);
+    //So that certificates aren't shown when the db loads before the blockchain query returns
+    TemplateVar.set(template, "isDeleted", true);
+    myContract = getContract(certificateAddress);
 
   TemplateVar.set(template, "certificateAddress", certificateAddress);
 
@@ -115,6 +67,18 @@ function getCertificateFromBlockchain(certificateAddress, template) {
   });
   myContract.sundryData(function(err, res) {
     TemplateVar.set(template, "sundryData", res);
+  });
+  myContract.isDeleted(function(err,res){
+      console.log(res);
+      TemplateVar.set(template, "isDeleted", res);
+  });
+}
+
+function deleteCertificate(certificateAddress) {
+  var myContract = getContract(certificateAddress);
+  myContract.deleteCertificate(function(err, res){
+      console.log(err);
+      if(!err) console.log("hi" + res);
   });
 }
 
@@ -131,13 +95,6 @@ Template.registerHelper("formatDate", function(date) {
   return moment(timeStamp).format("YYYY-MM-DD");
 });
 
-//   concatenate: function(v1, v2) {
-//     return v1.toString + v2.toString;
-//   }
-// });
-
-Template.registerHelper();
-
 Template.CandidateSearch.onCreated(function() {
   Session.set("certificateSearchResults", null);
   Session.set("searchType", "candidateNameDOB");
@@ -153,10 +110,15 @@ Template.ChildCertificate.onCreated(function() {
 Template.WalletBallance.helpers({
   getEthBallance: function() {
     var template = Template.instance();
-    TemplateVar.set(template, "walletAddress", Address);
-    web3.eth.getBalance(Address, function(err, res) {
-      ethBlance = web3.fromWei(res, "ether");
-      TemplateVar.set(template, "walletBallance", ethBlance);
+    web3.eth.getAccounts(function(err, res) {
+      if (!err) {
+        Address = res[0];
+        TemplateVar.set(template, "walletAddress", Address);
+        web3.eth.getBalance(Address, function(err, res) {
+          ethBlance = web3.fromWei(res, "ether");
+          TemplateVar.set(template, "walletBallance", ethBlance);
+        });
+      }
     });
   }
 });
@@ -258,5 +220,12 @@ Template.CreateNewCertificateForm.events({
     var sundryData = event.target.sundryData.value;
     createContract(candidateName, candidateDOB, sundryData);
     return false;
+  }
+});
+
+Template.DeleteCertificateForm.events({
+  "submit .deleteCertificateForm": function(event, t) {
+    var certificateAddress = event.target.certificateAddress.value;
+    deleteCertificate(certificateAddress);
   }
 });
