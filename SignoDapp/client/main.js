4234c45bedc958dbf37581ddde5d9daa1f0bb46e
Christopher Maree <christopher.maree@gmail.com>
2017-08-07 23:45:29 +0200

    Moved the charting utilities to server side. Added ability to filter by Week/month/3 months on the chart

--- SignoDapp/client/main.js
+++ SignoDapp/client/main.js
@@ -49,19 +49,13 @@ const ABI_ARRAY = [{
 }];
 const BYTE_CODE = "6060604052341561000f57600080fd5b6040516104b13803806104b1833981016040528080519060200190919080518201919050505b336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550816001816000191690555080600290805190602001906100969291906100ba565b506000600360006101000a81548160ff0219169083151502179055505b505061015f565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f106100fb57805160ff1916838001178555610129565b82800160010185558215610129579182015b8281111561012857825182559160200191906001019061010d565b5b509050610136919061013a565b5090565b61015c91905b80821115610158576000816000905550600101610140565b5090565b90565b6103438061016e6000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063083dfe681461006a5780635455e36b146100f9578063aa4107321461014e578063afa936b81461017f578063d7efb6b714610194575b600080fd5b341561007557600080fd5b61007d6101c1565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100be5780820151818401525b6020810190506100a2565b50505050905090810190601f1680156100eb5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b341561010457600080fd5b61010c61025f565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b341561015957600080fd5b610161610284565b60405180826000191660001916815260200191505060405180910390f35b341561018a57600080fd5b61019261028a565b005b341561019f57600080fd5b6101a7610304565b604051808215151515815260200191505060405180910390f35b60028054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156102575780601f1061022c57610100808354040283529160200191610257565b820191906000526020600020905b81548152906001019060200180831161023a57829003601f168201915b505050505081565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b60015481565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161415156102e557600080fd5b6001600360006101000a81548160ff0219169083151502179055505b5b565b600360009054906101000a900460ff16815600a165627a7a7230582009e651caf5d336c4233a93f7dbf345b82f62d77489c9a9cf4183f789750aacad0029";
 
+//global client variables
 let Address;
 
-// let addressPromise = new Promise((address) => {
-//     web3.eth.getAccounts(function (err, res) {
-//         if (!err) {
-//             Address = res[0];
-//             console.log(Address);
-//             address(res[0]);
-//         }
-//     });
-// });
-
 Certificates = new Mongo.Collection("certificates");
+CertificatesProducedGraph = new Mongo.Collection("certificatesProducedGraph");
+UniqueIssuerGraph = new Mongo.Collection("uniqueIssuerGraph");
+UniqueRecipientGraph = new Mongo.Collection("uniqueRecipientGraph");
 
 Meteor.startup(function () {
     Session.set("updateCertificateSearchResults", null);
@@ -486,14 +480,15 @@ Template.candidateDetailsSearch.onRendered(function () {
 
 Template.WalletBallance.onRendered(function () {
     Meteor.call('pageLoadCount', "IP");
+    template = Template.instance();
     //we need all the subscriptions to be loaded before we can populate the respective graphs
     let loadSubCount = 0;
-    let subscriptionCount = 3;
+    let subscriptionCount = 3; //number of subscriptions required to process the request
     Meteor.subscribe('certificatesProducedGraph', {
         onReady: function () {
             loadSubCount = loadSubCount + 1;
             if (loadSubCount === subscriptionCount) {
-                buildTimeGraph();
+                buildTimeGraph(30, template);
             }
         }
     });
@@ -501,7 +496,7 @@ Template.WalletBallance.onRendered(function () {
         onReady: function () {
             loadSubCount = loadSubCount + 1;
             if (loadSubCount === subscriptionCount) {
-                buildTimeGraph();
+                buildTimeGraph(30, template);
             }
         }
     });
@@ -509,25 +504,44 @@ Template.WalletBallance.onRendered(function () {
         onReady: function () {
             loadSubCount = loadSubCount + 1;
             if (loadSubCount === subscriptionCount) {
-                buildTimeGraph();
+                buildTimeGraph(30, template);
             }
         }
     });
 });
 
-function buildTimeGraph() {
-    CertificatesProducedGraph = new Mongo.Collection("certificatesProducedGraph");
-    let dateLabelsProduced = _.pluck(CertificatesProducedGraph.find().fetch(), "_id");
-    let dateValuesProduced = _.pluck(CertificatesProducedGraph.find().fetch(), "count");
+function buildTimeGraph(timeframe, template) {
+    Date.prototype.getWeek = function () {
+        var onejan = new Date(this.getFullYear(), 0, 1);
+        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+    };
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
 
-    UniqueIssuerGraph = new Mongo.Collection("uniqueIssuerGraph");
-    let dateValuesIssuer = _.pluck(UniqueIssuerGraph.find().fetch(), "count");
+    let dateObject = new Date();
+    let currentWeek = dateObject.getWeek();
+    let currentMonth = monthNames[dateObject.getMonth()];
+    let currentYear = dateObject.getFullYear();
+    let returnString;
 
-    UniqueRecipientGraph = new Mongo.Collection("uniqueRecipientGraph");
-    let dateValuesRecipient = _.pluck(UniqueRecipientGraph.find().fetch(), "count");
+    switch (timeframe) {
+        case 7:
+            returnString = ("Week " + currentWeek + ", " + currentMonth + " " + currentYear);
+            TemplateVar.set(template, "currentTimeFrame", returnString);
+            break;
+        case 30:
+            returnString = (currentMonth + " " + currentYear);
+            TemplateVar.set(template, "currentTimeFrame", returnString);
+            break;
+        case 90:
+            returnString = (currentYear);
+            TemplateVar.set(template, "currentTimeFrame", returnString);
+            break;
+    }
 
     let lineChartData = {
-        labels: dateLabelsProduced,
+        labels: _.pluck(CertificatesProducedGraph.find({}, {sort: {_id: 1}}).fetch().slice(-timeframe), "_id"),
         datasets: [
             {
                 label: 'Certificates',
@@ -535,7 +549,7 @@ function buildTimeGraph() {
                 borderColor: 'rgba(220,220,220,1)',
                 pointBackgroundColor: 'rgba(220,220,220,1)',
                 pointBorderColor: '#fff',
-                data: dateValuesProduced
+                data: _.pluck(CertificatesProducedGraph.find({}, {sort: {_id: 1}}).fetch().slice(-timeframe), "count")
             },
             {
                 label: 'Unique Issuers',
@@ -543,7 +557,7 @@ function buildTimeGraph() {
                 borderColor: 'rgba(151,187,205,1)',
                 pointBackgroundColor: 'rgba(151,187,205,1)',
                 pointBorderColor: '#fff',
-                data: dateValuesIssuer
+                data: _.pluck(UniqueIssuerGraph.find({}, {sort: {_id: 1}}).fetch().slice(-timeframe), "count")
             },
             {
                 label: 'Unique Recipients',
@@ -551,12 +565,12 @@ function buildTimeGraph() {
                 borderColor: 'rgba(205,196,151,1)',
                 pointBackgroundColor: 'rgba(205,196,151,1)',
                 pointBorderColor: '#fff',
-                data: dateValuesRecipient
+                data: _.pluck(UniqueRecipientGraph.find({}, {sort: {_id: 1}}).fetch().slice(-timeframe), "count")
             }
         ]
     };
-    let ctx = document.getElementById('timeGraph');
-    var chart = new Chart(ctx, {
+    let ctx = $('#timeGraph');
+    chart = new Chart(ctx, {
         type: 'line',
         data: lineChartData,
         options: {
@@ -564,6 +578,7 @@ function buildTimeGraph() {
         }
     });
 }
+
 
 Template.inputFields.onRendered(function (event) {
     if (this.data.type == 'date') {
@@ -710,6 +725,25 @@ Template.ChildCertificate.helpers({
 Template.UpdateCertificateForm.helpers({
     updateCertificateSearchResults: function () {
         return Session.get("updateCertificateSearchResults");
+    }
+});
+
+Template.WalletBallance.events({
+    'change': function (event) {
+        template = Template.instance();
+        switch (event.target.outerText) {
+            case "Week":
+                buildTimeGraph(7, template);
+                break;
+            case "Month":
+                buildTimeGraph(30, template);
+                break;
+            case "3 Months":
+                buildTimeGraph(90, template);
+                break;
+        }
+
+
     }
 });
 
--- SignoDapp/server/main.js
+++ SignoDapp/server/main.js
@@ -62,58 +62,19 @@ const ABI_ARRAY = [{
 
 const BYTE_CODE = "6060604052341561000f57600080fd5b6040516104b13803806104b1833981016040528080519060200190919080518201919050505b336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550816001816000191690555080600290805190602001906100969291906100ba565b506000600360006101000a81548160ff0219169083151502179055505b505061015f565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f106100fb57805160ff1916838001178555610129565b82800160010185558215610129579182015b8281111561012857825182559160200191906001019061010d565b5b509050610136919061013a565b5090565b61015c91905b80821115610158576000816000905550600101610140565b5090565b90565b6103438061016e6000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063083dfe681461006a5780635455e36b146100f9578063aa4107321461014e578063afa936b81461017f578063d7efb6b714610194575b600080fd5b341561007557600080fd5b61007d6101c1565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100be5780820151818401525b6020810190506100a2565b50505050905090810190601f1680156100eb5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b341561010457600080fd5b61010c61025f565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b341561015957600080fd5b610161610284565b60405180826000191660001916815260200191505060405180910390f35b341561018a57600080fd5b61019261028a565b005b341561019f57600080fd5b6101a7610304565b604051808215151515815260200191505060405180910390f35b60028054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156102575780601f1061022c57610100808354040283529160200191610257565b820191906000526020600020905b81548152906001019060200180831161023a57829003601f168201915b505050505081565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b60015481565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161415156102e557600080fd5b6001600360006101000a81548160ff0219169083151502179055505b5b565b600360009054906101000a900460ff16815600a165627a7a7230582009e651caf5d336c4233a93f7dbf345b82f62d77489c9a9cf4183f789750aacad0029";
 
+Meteor.setInterval(buildChart, 1000000);
+
 function getContract(certificateAddress) {
     return web3.eth.contract(ABI_ARRAY).at(certificateAddress);
 }
 
-Meteor.startup(() => {
-});
-
-Meteor.publish('theCertificates', function () {
-    return Certificates.find();
-});
-
-Meteor.publish('certificatesProducedGraph', function () {
-    return CertificatesProducedGraph.find();
-});
-
-Meteor.publish('uniqueIssuerGraph', function () {
-    return UniqueIssuerGraph.find();
-});
-
-Meteor.publish('uniqueRecipientGraph', function () {
-    return UniqueRecipientGraph.find();
-});
-
-Meteor.methods({
-    'insertCertificate': function (certificateAddress) {
-        if (BYTE_CODE.includes(web3.eth.getCode(certificateAddress).substr(2))) {
-            let timeStamp = Date.now() / 1000;
-            getCertificateFromBlockchain(certificateAddress).then((res, err) => {
-                    if (!err) {
-                        Certificates.insert({
-                            idHash: res["idHash"],
-                            certificateIssuer: res["certificateIssuer"],
-                            certificateAddress: certificateAddress,
-                            timeStamp: timeStamp
-                        });
-                        console.log("Entry successfully added to DB");
-                        contractCreationStatus = "Entry " + certificateAddress + " successfully added to db";
-                    }
-                }
-            );
-        } else {
-            console.log("Byte Codes Don't Match, it looks like you are doing something fishy");
-        }
-    },
-    'pageLoadCount': function (clientInfomation) {
-        console.log(clientInfomation);
-        // generateCertificatesProducedData();
-        // generateUniqueIssuerData();
-        // generateUniqueRecipientData();
-        // generateDashboardUsageGraph();
-    }
-});
+function buildChart() {
+    console.log("CHART REBUILD STARTED");
+    generateCertificatesProducedData();
+    generateUniqueIssuerData();
+    generateUniqueRecipientData();
+    generateDashboardUsageGraph();
+}
 
 function getCertificateFromBlockchain(certificateAddress) {
     return new Promise((resolve, error) => {
@@ -202,6 +163,7 @@ function generateUniqueIssuerData() {
         {$out: "uniqueIssuerData"}
     ]);
 }
+
 function generateUniqueRecipientData() {
     //First, select the new issuer addresses with their timeStamps. only take the min as this is first
     Certificates.aggregate([
@@ -278,7 +240,7 @@ function generateDashboardUsageGraph() {
 
     //x is the number of days for the period; 30 day chart. this checks if the map is missing the associated day.
     // if so, fill it in
-    for (let ms = endDateMilliseconds, x = 1; x < 30; x++) {
+    for (let ms = endDateMilliseconds, x = 1; x < 90; x++) {
         if (!( ms in mapProduced )) {
             mapProduced[ms] = {"_id": moment.unix((+new Date(ms)) / 1000).format("YYYY/MM/DD"), "count": 0};
         }
@@ -303,32 +265,72 @@ function generateDashboardUsageGraph() {
     var finalResultIssuer = [];
     var finalResultRecipient = [];
 
+    //as it stands, I dont know how to write the map to a collection. we go via an array!
     for (var x in mapProduced) {
-
         finalResultProduced.push(mapProduced[x]);
         finalResultIssuer.push(mapIssuer[x]);
         finalResultRecipient.push(mapRecipient[x]);
     }
-    finalResultProduced.sort(sortResult);
-    finalResultIssuer.sort(sortResult);
-    finalResultRecipient.sort(sortResult);
-
-    //as we are using a batch insert command, we don't need to iterate through the ordered list at this point
+        //as we are using a batch insert command, we don't need to iterate through the ordered list at this point
     CertificatesProducedGraph.batchInsert(finalResultProduced);
     UniqueIssuerGraph.batchInsert(finalResultIssuer);
     UniqueRecipientGraph.batchInsert(finalResultRecipient);
-    console.log("BUILD DONE");
+    // console.log(CertificatesProducedGraph.find().fetch());
+    console.log("CHART REBUILD DONE");
 }
 
-function sortResult(x, y) {
-    //function to sort by day entered
-    var t1 = x._id;
-    var t2 = y._id;
-    if (t1 < t2) {
-        return -1;
-    } else if (t1 == t2) {
-        return 0;
-    } else {
-        return 1;
+Meteor.startup(() => {
+});
+
+Meteor.publish('theCertificates', function () {
+    return Certificates.find();
+});
+
+Meteor.publish('certificatesProducedGraph', function () {
+    return CertificatesProducedGraph.find();
+});
+
+Meteor.publish('uniqueIssuerGraph', function () {
+    return UniqueIssuerGraph.find();
+});
+
+Meteor.publish('uniqueRecipientGraph', function () {
+    return UniqueRecipientGraph.find();
+});
+
+Meteor.methods({
+    'insertCertificate': function (certificateAddress) {
+        if (BYTE_CODE.includes(web3.eth.getCode(certificateAddress).substr(2))) {
+            let timeStamp = Date.now() / 1000;
+            getCertificateFromBlockchain(certificateAddress).then((res, err) => {
+                    if (!err) {
+                        Certificates.insert({
+                            idHash: res["idHash"],
+                            certificateIssuer: res["certificateIssuer"],
+                            certificateAddress: certificateAddress,
+                            timeStamp: timeStamp
+                        });
+                        console.log("Entry successfully added to DB");
+                        contractCreationStatus = "Entry " + certificateAddress + " successfully added to db";
+                    }
+                }
+            );
+        } else {
+            console.log("Byte Codes Don't Match, it looks like you are doing something fishy");
+        }
+    },
+    'pageLoadCount': function (clientInfomation) {
+        console.log(clientInfomation);
+        console.log(this.connection.clientAddress);
+        // buildChart();
     }
-}
+});
+
+// Meteor.users.find({ "status.online": true }).observe({
+//     added: function(id) {
+//         console.log("new User!");
+//     },
+//     removed: function(id) {
+//         console.log("User left");
+//     }
+// });
